755f6938e4b2d27cab452cfa9c1b70d88bab6626
Gaurav Bajaj <goru97@gmail.com>
2015-10-13 11:44:34 -0700

    Assumed final commit for query

--- metrics/v2/query/requests.go
+++ metrics/v2/query/requests.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
-const(
+const (
 	SUM = "sum"
 	MAX = "max"
 	MIN = "min"
@@ -14,7 +14,8 @@ const(
 	AVERAGE = "averge"
 )
 
-const(
+//Roll-ups process full-resolution data into coarser granularities of 5 minutes, 20 minutes, 60 minutes, 4 hours and 24 hours.
+const (
 	FULL = "full"
 	MIN5 = "min5"
 	MIN20 = "min20"
@@ -23,48 +24,59 @@ const(
 	MIN1440 = "min1440"
 )
 
-// GetDataForListByPoints retrieve data for a list of metrics for the specified tenant associated with RackspaceMetrics.
+// GetDataForListByPoints retrieve data against a list of metrics and number of points, for the specified tenant associated with RackspaceMetrics.
 func GetDataForListByPoints(c *gophercloud.ServiceClient, from int64, to int64, points int32, metrics ...string) (MetricListData, error) {
 	var res GetResult
 	reqBody := make([]interface{}, len(metrics))
 	for i, v := range metrics {
 		reqBody[i] = v
 	}
-	_, res.Err = c.Post(getDataForList(c, from, to, points),reqBody, &res.Body, nil)
+	_, res.Err = c.Post(getURLForListAndPoints(c, from, to, points), reqBody, &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{200},
+	})
 	b := res.Body.(interface{})
 	var metricList MetricListData
-	err := mapstructure.Decode(b,&metricList)
+	err := mapstructure.Decode(b, &metricList)
 	return metricList, err
 }
 
+// GetDataForListByResolution retrieve data against a list of metrics and specified resolution, for the specified tenant associated with RackspaceMetrics.
+func GetDataForListByResolution(c *gophercloud.ServiceClient, from int64, to int64, resolution string, metrics ...string) (MetricListData, error) {
+	var res GetResult
+	reqBody := make([]interface{}, len(metrics))
+	for i, v := range metrics {
+		reqBody[i] = v
+	}
+	_, res.Err = c.Post(getURLForListAndResolution(c, from, to, resolution), reqBody, &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{200},
+	})
+	b := res.Body.(interface{})
+	var metricList MetricListData
+	err := mapstructure.Decode(b, &metricList)
+	return metricList, err
+}
 
+// GetDataByPoints retrieve metric data by points, for the specified tenant associated with RackspaceMetrics.
 func GetDataByPoints(c *gophercloud.ServiceClient, metric string, from int64, to int64, points int32, sel ...string) GetResult {
 	var res GetResult
-		if len(sel) >= 1{
-			_, res.Err = c.Get(getURLForPointsWithSelect(c, metric, from, to, points, sel), &res.Body, nil)
-			return res
-		}
-		_, res.Err = c.Get(getURLForPoints(c, metric, from, to, points), &res.Body, nil)
+	if len(sel) >= 1 {
+		_, res.Err = c.Get(getURLForPointsWithSelect(c, metric, from, to, points, sel), &res.Body, nil)
 		return res
+	}
+	_, res.Err = c.Get(getURLForPoints(c, metric, from, to, points), &res.Body, nil)
+	return res
 }
 
+// GetDataByPoints retrieve metric data by resolution, for the specified tenant associated with RackspaceMetrics.
 func GetDataByResolution(c *gophercloud.ServiceClient, metric string, from int64, to int64, resolution string, sel ...string) GetResult {
 	var res GetResult
-		if len(sel) >= 1{
-			_, res.Err = c.Get(getURLForResolutionWithSelect(c, metric, from, to, resolution, sel), &res.Body, nil)
-			return res
-		}
-		_, res.Err = c.Get(getURLForResolution(c, metric, from, to, resolution), &res.Body, nil)
+	if len(sel) >= 1 {
+		_, res.Err = c.Get(getURLForResolutionWithSelect(c, metric, from, to, resolution, sel), &res.Body, nil)
 		return res
-}
-
-// Limits retrieves the number of API transaction that are available for the specified tenant associated with RackspaceMetrics.
-//TODO: Fix response code at Blueflood/Repose end
-/*func Limits(c *gophercloud.ServiceClient) GetResult {
-	var res GetResult
-	_, res.Err = c.Get(getLimits(c), &res.Body, nil)
+	}
+	_, res.Err = c.Get(getURLForResolution(c, metric, from, to, resolution), &res.Body, nil)
 	return res
-}*/
+}
 
 // SearchMetric retrieves a list of available metrics for the specified tenant associated with RackspaceMetrics.
 func SearchMetric(c *gophercloud.ServiceClient, metric string) ([]Metric, error) {
@@ -72,9 +84,10 @@ func SearchMetric(c *gophercloud.ServiceClient, metric string) ([]Metric, error)
 	_, res.Err = c.Get(getSearchURL(c, metric), &res.Body, nil)
 	b := res.Body.(interface{})
 	var metrics []Metric
-	err := mapstructure.Decode(b,&metrics)
+	err := mapstructure.Decode(b, &metrics)
 	return metrics, err
 }
+
 //GetEvents retrieves a list of events for the specified tenant associated with RackspaceMetrics.
 func GetEvents(c *gophercloud.ServiceClient, from int64, until int64, tag ...string) ([]Event, error) {
 	var res GetResult
@@ -85,6 +98,18 @@ func GetEvents(c *gophercloud.ServiceClient, from int64, until int64, tag ...str
 	}
 	b := res.Body.(interface{})
 	var events []Event
-	err := mapstructure.Decode(b,&events)
+	err := mapstructure.Decode(b, &events)
 	return events, err
+}
+
+// GetLimits retrieves the number of API transaction that are available for the specified tenant associated with RackspaceMetrics.
+func GetLimits(c *gophercloud.ServiceClient) (Limits, error) {
+	var res GetResult
+	_, res.Err = c.Get(getLimits(c), &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{200, 404},
+	})
+	b := res.Body.(map[string]interface{})
+	var limits Limits
+	err := mapstructure.Decode(b["limits"], &limits)
+	return limits, err
 }
